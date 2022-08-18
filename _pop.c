bd80302afdd2a63c8b3e9c8bc1a868248d4dd59c
root <piotr62@live.fr>
2022-08-18 05:23:19 -0700

    changing error handling and providing swap

--- _pop.c
+++ _pop.c
@@ -11,8 +11,9 @@ void _pop(stack_t **stack, unsigned int line_number)
 
 	if (*stack == NULL)
 	{
-		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
-		exit(EXIT_FAILURE);
+		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
+		error = 1;
+		return;
 	}
 	*stack = (*stack)->next;
 	if (*stack)
