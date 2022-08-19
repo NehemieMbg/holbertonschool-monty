1f4e7da5d7d450cb3c28c9de30853c07c68a6356
root <piotr62@live.fr>
2022-08-18 23:53:03 -0700

    first advanced

---
+++ _sub.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+
+/**
+ * _sub - subtracts top element of stack from the second element
+ *  at the end the stack is one element shorter
+ * @stack: double pointer on head of stack
+ * @line_number: line number of script file
+ */
+
+void _sub(stack_t **stack, unsigned int line_number)
+{
+	int ret;
+
+	if (*stack && (*stack)->next)
+	{
+		ret = (*stack)->n;
+		_pop(stack, line_number);
+		(*stack)->n -= ret;
+	}
+	else
+	{
+		fprintf(stderr, "L%u: can't sub, stack too short", line_number);
+		error = 1;
+		return;
+	}
+}
