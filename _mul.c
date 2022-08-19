27ca509e6ff92ab3651025691f450b754c491e88
NehemieMbg <nehemie.mbg@gmail.com>
2022-08-19 07:21:44 +0000

    Function that mul the two last nodes

---
+++ _mul.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+
+/**
+ * _mul - Function that mul the two last node of a linked list data
+ * @stack: Double pointer to the head of stack
+ * @line_number: line number of file we process
+ */
+void _mul(stack_t **stack, unsigned int line_number)
+{
+	int res = 0;
+
+	if (*stack && (*stack)->next)
+	{
+		res += (*stack)->n;
+		_pop(stack, line_number);
+		(*stack)->n *= res;
+	}
+	else
+	{
+		fprintf(stderr, "L%u: can't mul, stack too short\n", line_number);
+		error = 1;
+		return;
+	}
+}
+
